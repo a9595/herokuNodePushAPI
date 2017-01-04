6c450e6ace1843d244196a647f462e16aa055b04
Andrii Kovalchuk <tieorange@gmail.com>
2017-01-04 20:37:44 +0100

    req body log

--- push/index.js
+++ push/index.js
@@ -18,18 +18,20 @@ app.get('/', function (request, response) {
 
 
 app.post('/notification', function (req, res) {
+    console.log("Request body = ", req.body);
+
     var deviceTokenEmulator = "fs5O5HoGKok:APA91bGnj2dKlQ0QfFnCtRU9LvhQyTbWnnw5PFLqzR6emb7r1hTCuZiD6CnLJ7ntu9wts-hZXBSkjH8A1n6bsTHqzmeyznN9pbwO1I88co-GPKsrT5F_X6P3hguA1QcZzZTVWYddXqac"
-    var deviceTokenPixel = "cvmUMcMamrQ:APA91bHcvSV2xDUjqYT48g_CI3lxelIGtRwDbZrhkCVlj7L6Lrnn3pn9GlaQMT2CKRsMjyWAEsvaxD5skbLktpEuc8h7zMrES5vVK49LQy1zmwfArV6qMkKsq6oRnF1sSZT0nWlMbC33"
     var deviceToken5X = "cvmUMcMamrQ:APA91bHcvSV2xDUjqYT48g_CI3lxelIGtRwDbZrhkCVlj7L6Lrnn3pn9GlaQMT2CKRsMjyWAEsvaxD5skbLktpEuc8h7zMrES5vVK49LQy1zmwfArV6qMkKsq6oRnF1sSZT0nWlMbC33"
+    // var deviceToken5X = "cvmUMcMamrQ:APA91bHcvSV2xDUjqYT48g_CI3lxelIGtRwDbZrhkCVlj7L6Lrnn3pn9GlaQMT2CKRsMjyWAEsvaxD5skbLktpEuc8h7zMrES5vVK49LQy1zmwfArV6qMkKsq6oRnF1sSZT0nWlMbC33"
     var serverKey = "AAAA-Qaz-e8:APA91bHXGHIbaxoAYzSY0fWPsDA3fvNC9x0_Xa1ent9H9jYbYX-EciOOE3RrHGwMaLg_NeZ96hdZmH_H56uSgEFtNwsVZgQpZ03X-O3o3V5PMwHtHPPq37iDmNfvddBa6HDIk9gOWmGg74gHX0wg-snMas5RvFt0LA"
 
     var fcm = new FCM(serverKey);
 
     var mobile_tokens = [];
-    mobile_tokens.push(deviceTokenPixel);
+    mobile_tokens.push(deviceToken5X);
 
     var message = {
-        to: deviceTokenPixel,
+        to: deviceToken5X,
         notification: {
             title: "title",
             body: req.body
@@ -39,7 +41,7 @@ app.post('/notification', function (req, res) {
     fcm.send(message, function (err, response) {
         if (err) {
             console.log("Error!!!", err.message);
-            res.sendStatus(200);
+            res.sendStatus(500);
         } else {
             console.log("Success!!");
             res.sendStatus(200);
